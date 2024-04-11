a182b82a8e5d04c26f88b92b1e3aada31f0029e7
Blake Hansen <hansen.blake@student.greenriver.edu>
2024-04-11 13:59:23 -0400

    new controller

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// 328/pets/index.php
+// This is the controller
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+// Instantiate the F3 Base class
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Pets</h1>';
+
+    // Render a view page
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+// Run fat-free
+$f3->run();
