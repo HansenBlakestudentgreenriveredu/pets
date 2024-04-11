3c40cb23bb09065fafc8fa19bb01c4cdb8d48238
Blake Hansen <hansen.blake@student.greenriver.edu>
2024-04-11 14:16:57 -0400

    new styles css

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Pets</h1>';
+    //echo '<h1>Pets</h1>';
 
     // Render a view page
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run fat-free
